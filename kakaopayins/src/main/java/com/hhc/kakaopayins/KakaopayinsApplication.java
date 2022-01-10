d6c6ecd83d613a972d803fa0b6b21bb7ca057a5c
heechu85 <heechun85@naver.com>
2022-01-11 00:38:40 +0900

    프로젝트 생성

---
+++ kakaopayins/src/main/java/com/hhc/kakaopayins/KakaopayinsApplication.java
@@ -0,0 +1,13 @@
+package com.hhc.kakaopayins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaopayinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaopayinsApplication.class, args);
+	}
+
+}
---
+++ kakaopayins/src/test/java/com/hhc/kakaopayins/KakaopayinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhc.kakaopayins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaopayinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
